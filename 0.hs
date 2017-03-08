3f3b0836b9830c85ddd0c36d2b1e3d7723c77c3e
Hwi-yong <telnturtle@gmail.com>
2017-03-08 22:36:59 +0900

    변경

--- 0.hs
+++ 0.hs
@@ -1,8 +1,7 @@
--- SIC assembler by Goubugyi
--- main = putStrLn "Bonjour, world!"
+-- SIC assembler by turtle
 import System.IO
-import Data.List.Split
--- fn: splitOn
+import Data.List.Split -- fn: splitOn
+import Data.List -- B.isPrefixOf
 
 
 
@@ -37,8 +36,7 @@ instToOpcode inst = case inst of
     "WD"    -> "DC"
 
 consPassOne :: [String] -> PassOne -> PassOne
-consPassOne l1 (PassOne l2 s locctr) = 
-    PassOne ([l1] ++ l2) s locctr
+consPassOne l1 (PassOne l2 s locctr) = PassOne ([l1] ++ l2) s locctr
 
 hexChar :: Char -> Int
 hexChar ch
@@ -73,12 +71,12 @@ hexToDec hxStr = go (reverse hxStr)
 
 fn0 :: PassOne -> PassOne
 fn0 arg@(PassOne [] _ _ ) = arg
+
 fn0 (PassOne (l:ls) ss locctr) = 
     if ( l !! 0 ) !! 0 == '.' then fn0 (PassOne ls ss locctr) else
-        if l !! 1 /= "end" then (PassOne (l:ls) ss locctr) else
+        if l !! 1 \= "end" then (PassOne (l:ls) ss locctr) else
             let sVal = if l !! 0 == "" then ss else ss ++ [SymType (l !! 2) ( show locctr)]
-                in
-                    case l !! 1 of 
+                in  case l !! 1 of 
                         "word" -> (l ++ [show locctr]) `consPassOne` fn0 ( PassOne ls sVal (locctr + 3) )
                         "resw" -> (l ++ [show locctr]) `consPassOne` fn0 ( PassOne ls sVal (locctr + 3* read (l !! 2)) )
                         "resb" -> (l ++ [show locctr]) `consPassOne` fn0 ( PassOne ls sVal (locctr + read (l !! 2)) )
@@ -87,20 +85,8 @@ fn0 (PassOne (l:ls) ss locctr) =
                         _      -> fn0 ( PassOne ls sVal (locctr + 3) )
                         -- _(inst in optab)-> add 3(instruction length) to locctr
 
-listOfPassOne :: PassOne -> [[String]]
-listOfPassOne (PassOne list _ _) = list
-myListOfPassOne :: PassOne -> [String]
-myListOfPassOne p = map fn $ listOfPassOne p
-    where
-        fn :: [String] -> String
-        fn [] = []
-        fn (s:ss) = s ++ "\t" ++ fn ss
--- passOne = 
---     something
---         where
---             fn0 :: 
---             fn0 = 
---                 if 
+
+
 
 -- pass one 
 -- end 줄의 처리는 하지 않았음
@@ -114,21 +100,41 @@ myListOfPassOne p = map fn $ listOfPassOne p
 -- 해시 어쩌구 안쓰냐 -> 나중에 쓰자
 
 data SymType = SymType { name :: String , address :: String }
--- data PassOne = PassOne String (Maybe SymType) Int
 data PassOne = PassOne [[String]] [SymType] Int
 
+
+
 main = do
-    putStr "enter file name: "
+    putStrLn "enter file name: "
     sourceFileName <- getLine
     handleS <- openFile sourceFileName ReadMode
     contentsS <- hGetContents handleS
-    let --parsedByTab = [head parsedByTabs0, ]
-        -- where parsedByTabs0 = splitOn "\t" $ splitOn "\n" contentsS
-        parsedByTabs0 = map (splitOn "\t") $ splitOn "\n" contentsS
+    
+    let parsedByTabs0 = map (splitOn "\t") $ splitOn "\n" contentsS
         starting_address = if head parsedByTabs0 !! 1 == "start" then
             head $ read $ head parsedByTabs0 !! 2 else 0
         afterPassOne = PassOne parsedByTabs0 [] starting_address
-    -- putStr "hi"
+    
     mapM putStrLn (myListOfPassOne afterPassOne)
-    putStrLn "어?"
-    mapM putStrLn (map (\(SymType s1 s2) -> s1 ++ ", " ++ s2) ((\(PassOne _ ss _ ) -> ss) afterPassOne))
+    putStrLn "\nhi"
+    mapM putStrLn $ map strSymType (symTypeofPassOne afterPassOne)
+
+
+
+
+
+
+
+myListOfPassOne :: PassOne -> [String]
+myListOfPassOne p = map fn $ listOfPassOne p
+    where
+        listOfPassOne :: PassOne -> [[String]]
+        listOfPassOne (PassOne list _ _) = list
+
+        fn :: [String] -> String
+        fn [] = []
+        fn (s:ss) = s ++ "\t" ++ fn ss
+
+
+strSymType = \(SymType name addr) -> name ++ ", " ++ addr
+symTypeofPassOne (PassOne l s loc) = s
---
+++ README.md
@@ -0,0 +1,3 @@
+# SIC Assembler syssoftware
+
+Porting to Python and Haskell of final project of System Software DD748 Kyonggi University spring 2016
